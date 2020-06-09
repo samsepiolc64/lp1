e2263588a23289e1d486f1923aa4732b069e4060
samsepiolc64 <40030079+samsepiolc64@users.noreply.github.com>
2020-06-09 15:23:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,253 @@
+﻿<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,600i,700,800&amp;subset=latin-ext" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<link href="/media/pl-komputronik/staticPages/2018_fibaro_philips/main.css" rel="stylesheet">
+<div id="main-lp">
+
+	<section id='header-lp'>
+		<div class='column first c-50'>
+			<div class='inside-column'>
+				<img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/kt-logo.png' class='client-logo'/>
+				<div style='clear:both;'></div>
+				<div class='first-white'>
+					<h1>DLA TYCH, KTÓRZY...</h1>
+					<div class='header-lp-copy'>szukają inteligentnych rozwiązań </div>
+				</div>
+				<div style='clear:both;'></div>
+				<!--<a href='#how-it-works' class='more-button more-blue'>Sprawdź</a>-->
+			</div>
+		</div>
+		<div class='column second c-50'>
+		</div>
+		<div style='clear:both;'></div>
+	</section>
+
+	<section id='how-it-works'>
+		<div class='content-lp'>
+			<h2><br>Jak skorzystać z promocji?</h2>
+			<ul id='counting'>
+				<li class='c-33'>
+					<div class='count-number2'>1</div>
+					<div class='count-title'>Przejdź na kartę interesującego<br>Cię produktu.</div>
+				</li>
+				<li class='c-33'>
+					<div class='count-number2'>2</div>
+					<div class='count-title'>Na stronie wybranego produktu<br>w sekcji „POLECANE ZESTAWY” <br>kliknij przy wybranym zestawie przycisk „Do koszyka”</div>
+				</li>
+				<li class='c-33'>
+					<div class='count-number2'>3</div>
+					<div class='count-title'>Oszczędź 15%!</div>
+				</li>
+			</ul>
+			<div style='clear:both;'></div>
+		</div>
+	</section>
+
+	<section id='description-lp'>
+		<div class='content-lp'>Wybierz zestaw i oszczędź 15%! </div>
+	</section>
+
+	<section id='how-it-works' class='gray'>
+		<div class='content-lp'>
+			<h2>Zestaw 1</h2>
+			<ul id='counting'>
+				<li class='c-33'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z1.png'>
+					</div>
+					<div class='count-title'>Philips Hue zestaw startowy + 3 grzybki E27, 9W, RGBW</div>
+					<a href='#prod_1' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+				<li class='c-33'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z2.png'>
+					</div>
+					<div class='count-title'>Fibaro plug </div>
+					<a href='#prod_4' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+				<li class='c-33'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z3.png'>
+					</div>
+					<div class='count-title'>Fibaro button</div>
+					<a href='#prod_5' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id='addsec' class='gray'>
+		<div class='content-lp'>
+			<a href='https://www.komputronik.pl/product/456767/philips-hue-zestaw-startowy-3-grzybki-e27-9w-rgbw.html ' class='more-button more-blue'>Kup zestaw</a>
+			<!--<a href='#how-it-works' class='more-button more-blue'>Rejestracja</a>-->
+		</div>
+	</section>
+
+	<section id='how-it-works'>
+		<div class='content-lp'>
+			<h2>Zestaw 2</h2>
+			<ul id='counting'>
+				<li class='c-50'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z4.png'>
+					</div>
+					<div class='count-title'>Philips Hue mostek </div>
+					<a href='#prod_2' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+				<li class='c-50'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z5.png'>
+					</div>
+					<div class='count-title'>Philips Hue taśma <br>Lightstrip RGBW z wtyczką </div>
+					<a href='#prod_3' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+				<li class='c-50'>
+					<div class='count-number'>
+						<img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z2.png'>
+					</div>
+					<div class='count-title'>Fibaro plug</div>
+					<a href='#prod_4' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+				<li class='c-50'>
+					<div class='count-number'><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z3.png'>
+					</div>
+					<div class='count-title'>Fibaro button</div>
+					<a href='#prod_5' class='more-button more-blue small-button'>Dowiedz się więcej</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id='addsec'>
+		<div class='content-lp'>
+			<a href='https://www.komputronik.pl/product/376149/philips-hue-mostek-applehomekit.html' class='more-button more-blue buy-button'>Kup zestaw</a>
+			<!--<a href='#how-it-works' class='more-button more-blue'>Rejestracja</a>-->
+		</div>
+	</section>
+
+	<section id='description-lp'>
+		<div class='content-lp'>Odkryj inteligentne rozwiązania dla Twojego domu </div>
+	</section>
+
+	<section id='products-lp' class='gray'>
+		<ul id='products-lp-list'>
+			<!-- Produkt 1 -->
+			<li class='right-layout' id='prod_1'>
+				<div class='content-lp'>
+					<div class='product-left'>
+						<iframe src="https://www.youtube.com/embed/cCHOBxb2q5I?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
+					<div class='product-right'>
+						<a href='#'><h3>Philips Hue zestaw startowy + 3 grzybki E27, 9W, RGBW</h3></a>
+						<table class='specification'>
+							<tr></tr>
+							<tr>
+								<td>Zestaw Philips Hue zapewniający białe i kolorowe nastrojowe oświetlenie zawiera 3 źródła światła, mostek i przełącznik. Dzięki kolorowemu i białemu oświetleniu możesz stworzyć niezwykłą atmosferę w swoim domu. Połączony system oświetlenia domowego umożliwia synchronizację kolorowych świateł z muzyką, programami telewizyjnymi i grami.</td>
+							</tr>
+						</table>
+						<div style='clear:both;'></div>
+						<div class="product-photo-box product-photo-desc"><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z1.png' class='product-photo'>
+						</div>
+						<div style='clear:both;'></div>
+						<a href='https://www.komputronik.pl/product/456767/philips-hue-zestaw-startowy-3-grzybki-e27-9w-rgbw.html ' class='more-button more-blue buy-button'>Kup teraz</a>
+					</div>
+				</div>
+			</li>
+			<!-- Produkt 2 -->
+			<li class='right-layout' id='prod_2'>
+				<div class='content-lp'>
+					<div class='product-left'>
+						<a href='#'><h3>Philips Hue mostek        </h3></a>
+						<table class='specification'>
+							<tr>
+								<td>Mostek Philips Hue to wszystko, czego potrzebujesz do zbudowania własnego systemu Philips Hue. Jest on „mózgiem” całej operacji, który umożliwia sterowanie wszystkimi produktami Philips Hue za pomocą aplikacji Philips Hue. Po zainstalowaniu mostka Philips Hue i pobraniu aplikacji Philips Hue na urządzenie przenośne możesz uwolnić wyobraźnię i zbudować własny system! </td>
+							</tr>
+						</table>
+						<div style='clear:both;'></div>
+						<div class="product-photo-box product-photo-desc"><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z4.png' class='product-photo'>
+						</div>
+						<div style='clear:both;'></div>
+						<a href='https://www.komputronik.pl/product/376149/philips-hue-mostek-applehomekit.html ' class='more-button more-blue buy-button'>Kup teraz</a>
+					</div>
+					<div class='product-right'>
+						<iframe src="https://www.youtube.com/embed/aTa26eNlI54?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
+				</div>
+			</li>
+			<!-- Produkt 3 -->
+			<li class='right-layout' id='prod_3'>
+				<div class='content-lp'>
+					<div class='product-left'>
+						<iframe src="https://www.youtube.com/embed/8ziHoz0xa6k?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
+					<div class='product-right'>
+						<a href='#'><h3>Philips Hue taśma Lightstrip RGBW z wtyczką   </h3></a>
+						<table class='specification'>
+							<tr></tr>
+							<tr>
+								<td>Uruchom wyobraźnię i kształtuj światło, jak tylko chcesz. Dodaj taśmę LightStrip Plus do systemu Philips Hue i stwórz imponujący efekt pod blatem lub szafką oraz za sprzętem audio-wideo. Taśmę Lightstrip Plus można zginać, przycinać i wydłużać, aby dostosować ją do swoich pomysłów.</td>
+							</tr>
+						</table>
+						<div style='clear:both;'></div>
+						<div class="product-photo-box product-photo-desc"><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z5.png' class='product-photo'>
+						</div>
+						<div style='clear:both;'></div>
+						<a href='https://www.komputronik.pl/product/376196/philips-hue-tasma-lightstrip-rgbw-zestaw-startowy.html  ' class='more-button more-blue buy-button'>Kup teraz</a>
+					</div>
+				</div>
+			</li>
+			<!-- Produkt 4 -->
+			<li class='right-layout' id='prod_4'>
+				<div class='content-lp'>
+					<div class='product-left'>
+						<a href='#'><h3>FIBARO Wall Plug </h3></a>
+						<table class='specification'>
+							<tr>
+								<td>FIBARO Wall Plug to niezwykle funkcjonalne urządzenie w formie przejściówki podłączanej bezpośrednio do dowolnego gniazda zasilającego, które znajdzie zastosowanie wszędzie tam, gdzie chcemy w wygodny i bezobsługowy sposób zdalnie zarządzać urządzeniami elektrycznymi. Możesz więc włączać i wyłączać dowolną lampę stojącą lub inne urządzenia elektryczne z poziomu jednej aplikacji, gdziekolwiek jesteś. Kryształowy pierścień LED zmienia swój kolor w zależności od mocy pobieranej przez podłączone urządzenie informuje o zużywanej przez nie energii. </td>
+							</tr>
+						</table>
+						<div style='clear:both;'></div>
+						<div class="product-photo-box product-photo-desc"><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z2.png' class='product-photo'>
+						</div>
+						<div style='clear:both;'></div>
+						<a href='https://www.komputronik.pl/product/482118/fibaro-homekit-fgbwhwpe-102.html' class='more-button more-blue buy-button'>Kup teraz</a>
+					</div>
+					<div class='product-right'>
+						<iframe src="https://www.youtube.com/embed/ZKIBC3EPqek?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
+				</div>
+			</li>
+			<!-- Produkt 5 -->
+			<li class='right-layout' id='prod_5'>
+				<div class='content-lp'>
+					<div class='product-left'>
+						<iframe src="https://www.youtube.com/embed/dU9yxnitXXw?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					</div>
+					<div class='product-right'>
+						<a href='#'><h3>FIBARO The Button </h3></a>
+						<table class='specification'>
+							<tr></tr>
+							<tr>
+								<td>Pozwala na zarządzanie dowolnymi urządzeniami i uruchamianie scen w Twoim domu. Jednym kliknięciem wyłącz światło, dwoma uruchom wentylator, przytrzymaj, by uruchomić scenę. The Button jest wygodny w montażu, ponieważ można umieścić go w dowolnej pozycji, na każdej powierzchni w domu. Zsynchronizuj The Button z systemem FIBARO oraz podłącz go z innymi urządzeniami np. z oświetleniem Phillps Hue. Od teraz możesz korzystać z wachlarza funkcjonalności naszego systemu, dającego szerokie spektrum możliwości automatyzacji tam, gdzie najbardziej tego potrzebujesz. </td>
+							</tr>
+						</table>
+						<div style='clear:both;'></div>
+						<div class="product-photo-box product-photo-desc"><img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/z3.png' class='product-photo'>
+						</div>
+						<div style='clear:both;'></div>
+						<a href='https://www.komputronik.pl/product/482121/fibaro-homekit-fgbhpb-101-1.html ' class='more-button more-blue buy-button'>Kup teraz</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div style='clear:both;'></div>
+	</section>
+
+	<section id='foot'>
+		<div class='content-lp'>
+			<h2>Masz pytania?</h2>
+			<div class='desc'>Specjaliści Komputronik SA chętnie odpowiedzą na Twoje pytania.<br/> Skontaktuj się mailowo lub osobiście z handlowcem w salonie Komputronik lub zadzwoń na infolinię.
+			</div>
+			<div class='phone'>
+				<img src='/media/pl-komputronik/staticPages/2018_fibaro_philips/img/headphones.png' class='headphones'/><a href='tel:616680007'>61 668 00 07</a>
+			</div>
+			<div class='desc'><span class='infolinia'>Infolinia czynna:</span><br/> poniedziałek - piątek: 08:00 - 21:00, sobota: 09:00 - 16:00<br/> e-mail: <a href='mailto:sklep@komputronik.pl'>sklep@komputronik.pl</a><br/><br/> Sprawdź kontakt do salonów na <a href='http://www.komputronik.pl/kontakty'>www.komputronik.pl/kontakty</a>
+			</div>
+			<div style='clear:both;'></div>
+		</div>
+	</section>
+</div>
